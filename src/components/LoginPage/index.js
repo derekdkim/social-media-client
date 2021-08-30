89c62c1e7a7c18af6340e0b96d886fb7bda6b385
Derek Kim <ddkim1024@gmail.com>
2021-08-29 20:27:58 -0500

    Migrated LoginPage and SignupPage to react hook form validation

--- src/components/LoginPage/index.js
+++ src/components/LoginPage/index.js
@@ -1,66 +1,60 @@
 import axios from 'axios';
 import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 import { useAuthContext } from '../../context/AuthContextProvider';
 import { useStatusContext } from '../../context/StatusContextProvider';
 
 const LoginPage = () => {
-  const [username, setUsername] = useState();
-  const [password, setPassword] = useState();
   const [JWTErrMsg, setJWTErrMsg] = useState('');
   
   const auth = useAuthContext();
   const status = useStatusContext();
+  
+  const { register, handleSubmit, formState: { isValid } } = useForm({
+    mode: 'onChange'
+  });
 
-  const updateUsername = (event) => {
-    setUsername({
-      value: event.target.value
-    });
-  }
-
-  const updatePassword = (event) => {
-    setPassword({
-      value: event.target.value
-    });
-  }
-
-  const handleSubmit = async (event) => {
+  const attemptLogin = async (data, event) => {
     // Prevent page refresh
     event.preventDefault();
 
-    // Start Loading
-    status.setIsLoading(true);
+    if (isValid) {
+      // Start Loading
+      status.setIsLoading(true);
 
-    axios.post('https://journey-social-media-server.herokuapp.com/users/log-in', {
-        username: username.value,
-        password: password.value
-      })
-      .then(res => {
-        if (res.data.token) {
-          // Save JWT to localStorage
-          localStorage.setItem('token', res.data.token);
-          // Save JWT to auth Context
-          auth.setJWT(res.data.token);
-          // Save current user UUID to auth Context -- Important for recognizing if user is a participant or author
-          auth.setUUID(res.data.user.uuid);
-          // [CHANGE LATER]: Placeholder indicator for being logged in
-          auth.setLoggedIn(true);
-        } else {
-          setJWTErrMsg('Web Token not found.');
-          auth.setLoggedIn(false);
+      axios.post('https://journey-social-media-server.herokuapp.com/users/log-in', {
+          username: data.username,
+          password: data.password
+        })
+        .then(res => {
+          if (res.data.token) {
+            // Save JWT to localStorage
+            localStorage.setItem('token', res.data.token);
+            // Save JWT to auth Context
+            auth.setJWT(res.data.token);
+            // Save current user UUID to auth Context -- Important for recognizing if user is a participant or author
+            auth.setUUID(res.data.user.uuid);
+            // [CHANGE LATER]: Placeholder indicator for being logged in
+            auth.setLoggedIn(true);
+          } else {
+            setJWTErrMsg('Login failed. Please try again.');
+            auth.setLoggedIn(false);
+            status.setIsLoading(false);
+          }
+
+          // Finish Loading
           status.setIsLoading(false);
-        }
+        })
+        .catch(err => {
+          console.log(err);
+          setJWTErrMsg('Login failed. Please try again.');
 
-        // Finish Loading
-        status.setIsLoading(false);
-      })
-      .catch(err => {
-        console.log(err);
-
-        // Finish Loading
-        status.setIsLoading(false);
-      })
+          // Finish Loading
+          status.setIsLoading(false);
+        })
+    }
   }
 
   return (
@@ -69,37 +63,39 @@ const LoginPage = () => {
       <div className='one-tab-container content-panel form-container only-tab'>
         <form 
           className='auth-form'
-          onSubmit={handleSubmit}
+          onSubmit={ handleSubmit(attemptLogin) }
           noValidate
         >
           <h1 className='tab-heading text-center'>Welcome Back</h1>
 
-          <span className='text-input-error'>{JWTErrMsg}</span>
+          <span className='text-input-error'>{ JWTErrMsg }</span>
           
           <div className='input-container'>
             <label htmlFor='login-username-input'>Username</label>
             <input
+              {...register('username', {
+                required: 'Username cannot be blank'
+              })}
               type='text'
               className='text-input'
               id='login-username-input'
-              onChange={updateUsername}
-              required
             ></input>
           </div>
 
           <div className='input-container'>
             <label htmlFor='login-password-input'>Password</label>
             <input
+              {...register('password', {
+                required: 'Password cannot be blank'
+              })}
               type='password'
               className='text-input'
               id='login-password-input'
-              onChange={updatePassword}
-              required
             ></input>
           </div>
 
           <div className='btn-container'>
-            <button className='button'>Log In</button>
+            <button className={ isValid ? 'button' : 'button disabled-btn' }>Log In</button>
           </div>
         </form>
       </div>
--- src/components/SignupPage/index.js
+++ src/components/SignupPage/index.js
@@ -1,180 +1,72 @@
 import axios from 'axios';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Redirect } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 import './index.css';
 
 import { useStatusContext } from '../../context/StatusContextProvider';
 
 const SignupPage = () => {
-  const [username, setUsername] = useState({errMsg: ''});
-  const [password, setPassword] = useState({errMsg: ''});
-  const [firstName, setFirstName] = useState({errMsg: ''});
-  const [lastName, setLastName] = useState({errMsg: ''});
-  const [birthday, setBirthday] = useState({errMsg: ''});
-  const [formCompleted, setFormCompleted] = useState(false);
   const [formAccepted, setFormAccepted] = useState(false);
   const [formErrMsg, setFormErrMsg] = useState('');
 
   const status = useStatusContext();
-  
-  const updateUsername = (event) => {
-    // Validation check
-    let currValidity = true;
-    let currErrMsg = '';
-    const currValue = event.target.value;
+  const { register, handleSubmit, formState: { errors, isValid } } = useForm({
+    mode: 'onChange'
+  })
 
-    // Length check
-    if (currValue.length < 6 || currValue.length > 20) {
-      currErrMsg = 'Username must be between 6 and 20 characters';
-      currValidity = false;
-    }
-
-    // Alphanumeric check
-    if (!(/^[0-9a-z]+$/i.test(currValue))) {
-      currErrMsg = 'Username must consist of alphanumeric (A-Z and 0-9) characters.';
-      currValidity = false;
-    }
-
-    setUsername({
-        value: currValue,
-        valid: currValidity,
-        errMsg: currErrMsg
-    });
-  }
-
-  const updatePassword = (event) => {
-    // Validation check
-    let currValidity = true;
-    let currErrMsg = '';
-    const currValue = event.target.value;
-
-    // Length check
-    if (currValue.length < 8 || currValue.length > 22) {
-      currErrMsg = 'Password must be between 8 and 22 characters';
-      currValidity = false;
-    }
-
-    setPassword({
-        value: currValue,
-        valid: currValidity,
-        errMsg: currErrMsg
-    });
-  }
-
-  const updateFirstName = (event) => {
-    // Validation check
-    let currValidity = true;
-    let currErrMsg = '';
-    const currValue = event.target.value;
-
-    // Length check
-    if (currValue.length < 1) {
-      currErrMsg = 'Name must not be blank';
-      currValidity = false;
-    }
-
-    // Character check
-    if (!(/^[a-z]+$/i.test(currValue))) {
-      currErrMsg = 'Name must consist of alphabetical letters.';
-      currValidity = false;
-    }
-
-    setFirstName({
-        value: currValue,
-        valid: currValidity,
-        errMsg: currErrMsg
-    });
-  }
-
-  const updateLastName = (event) => {
-    // Validation check
-    let currValidity = true;
-    let currErrMsg = '';
-    const currValue = event.target.value;
-
-    // Length check
-    if (currValue.length < 1) {
-      currErrMsg = 'Name must not be blank';
-      currValidity = false;
-    }
-
-    // Character check
-    if (!(/^[a-z]+$/i.test(currValue))) {
-      currErrMsg = 'Name must consist of alphabetical letters.';
-      currValidity = false;
-    }
-
-    setLastName({
-        value: currValue,
-        valid: currValidity,
-        errMsg: currErrMsg
-    });
-  }
-
-  const updateBirthday = (event) => {
-    // Validation check
-    let currValidity = true;
-    let currErrMsg = '';
-    const currValue = event.target.value;
-
-    setBirthday({
-        value: currValue,
-        valid: currValidity,
-        errMsg: currErrMsg
-    });
-  }
-
-  const handleSubmit = async (event) => {
+  const attemptSignup = async (data, event) => {
     //Prevent page refresh after form submit
     event.preventDefault();
 
-    // Start Loading
-    status.setIsLoading(true);
+    if (isValid) {
+      // Start Loading
+      status.setIsLoading(true);
 
-    // Post to API
-    await axios.post('https://journey-social-media-server.herokuapp.com/users/sign-up', {
-        username: username.value,
-        password: password.value,
-        firstName: firstName.value,
-        lastName: lastName.value,
-        birthDate: birthday.value
-      })
-      .then(res => {
-        if (res.data.message === 'Sign-up successful') {
-          // Set state for redirect following submission if sign-up was successful
-          setFormAccepted(true);
-        } else {
-          // Add something to show error
+      // Fix error where JS causes the date to be off by one day if no time is added
+      const formattedBirthDate = data.birthDate + 'T00:00:00'; 
+
+      // Post to API
+      await axios.post('https://journey-social-media-server.herokuapp.com/users/sign-up', {
+          username: data.username,
+          password: data.password,
+          firstName: data.firstName,
+          lastName: data.lastName,
+          birthDate: new Date(formattedBirthDate)
+        })
+        .then(res => {
+          if (res.data.message === 'Sign-up successful') {
+            // Set state for redirect following submission if sign-up was successful
+            setFormAccepted(true);
+          } else {
+            // Add something to show error
+            setFormErrMsg('Sign-up failed. Please try again.');
+          }
+
+          // Loading Complete
+          status.setIsLoading(false);
+        })
+        .catch(err => {
+          console.log(err);
           setFormErrMsg('Sign-up failed. Please try again.');
-        }
 
-        // Loading Complete
-        status.setIsLoading(false);
-      })
-      .catch(err => {
-        console.log(err);
-
-        // Loading Complete
-        status.setIsLoading(false);
-      });
-  }
-
-  useEffect(() => {
-    // Check for form validation every time it gets updated
-    if (username.valid && password.valid && firstName.valid && lastName.valid && birthday.valid) {
-      setFormCompleted(true);
-    } else {
-      setFormCompleted(false);
+          // Loading Complete
+          status.setIsLoading(false);
+        });
     }
-  }, [username, password, firstName, lastName, birthday]);
+  }
 
   return (
     <div className='page-container'>
-      { formAccepted ? <Redirect to='/log-in'/> : <div></div> }
+      { /* Redirect if form was submitted successfully */ 
+        formAccepted && 
+        <Redirect to='/log-in'/> 
+      }
       <div className='one-tab-container form-container content-panel only-tab'>
         <form 
           className='auth-form'
-          onSubmit={handleSubmit}
+          onSubmit={ handleSubmit(attemptSignup) }
           noValidate
         >
           <h1 className='tab-heading text-center'>Create a New Account</h1>
@@ -184,68 +76,148 @@ const SignupPage = () => {
           <div className='input-container'>
             <label htmlFor='signup-username-input'>Username</label>
             <input
+              {...register('username', {
+                required: 'Username cannot be blank',
+                minLength: {
+                  value: 6,
+                  message: 'Username cannot be smaller than 6 characters.'
+                },
+                maxLength : {
+                  value: 20,
+                  message: 'Username cannot be larger than 20 characters.'
+                },
+                pattern: {
+                  value: /[0-9a-z]/,
+                  message: 'Username must consist of alphanumeric (A-Z and 0-9) characters.'
+                }
+              })}
               type='text'
               className='text-input'
               id='signup-username-input'
-              onChange={updateUsername}
-              required
             ></input>
-            <span className='text-input-error'>{username.errMsg}</span>
+            <ErrorMessage errors={ errors } name="username">
+              {({ messages }) =>
+                messages &&
+                Object.entries(messages).map(([type, message]) => (
+                  <p key={type} className='text-input-error'>{message}</p>
+                ))
+              }
+            </ErrorMessage>
           </div>
 
           <div className='input-container'>
             <label htmlFor='signup-password-input'>Password</label>
             <input
+              {...register('password', {
+                required: 'Password cannot be blank',
+                minLength: {
+                  value: 8,
+                  message: 'Password cannot be smaller than 8 characters.'
+                },
+                maxLength : {
+                  value: 22,
+                  message: 'Password cannot be larger than 22 characters.'
+                }
+              })}
               type='password'
               className='text-input'
               id='signup-password-input'
-              onChange={updatePassword}
-              required
             ></input>
-            <span className='text-input-error'>{password.errMsg}</span>
+            <ErrorMessage errors={ errors } name="password">
+              {({ messages }) =>
+                messages &&
+                Object.entries(messages).map(([type, message]) => (
+                  <p key={type} className='text-input-error'>{message}</p>
+                ))
+              }
+            </ErrorMessage>
           </div>
 
           <div className='input-container'>
             <label htmlFor='signup-firstname-input'>First Name</label>
             <input
+              {...register('firstName', {
+                required: 'First name cannot be blank',
+                maxLength : {
+                  value: 30,
+                  message: 'First name cannot be larger than 30 characters.'
+                },
+                pattern: {
+                  value: /[0-9a-z]/,
+                  message: 'First name must consist of alphanumeric (A-Z and 0-9) characters.'
+                }
+              })}
               type='text'
               className='text-input'
               id='signup-firstname-input'
-              onChange={updateFirstName}
-              required
             ></input>
-            <span className='text-input-error'>{firstName.errMsg}</span>
+            <ErrorMessage errors={ errors } name="firstName">
+              {({ messages }) =>
+                messages &&
+                Object.entries(messages).map(([type, message]) => (
+                  <p key={type} className='text-input-error'>{message}</p>
+                ))
+              }
+            </ErrorMessage>
           </div>
 
           <div className='input-container'>
             <label htmlFor='signup-lastname-input'>Last Name</label>
             <input
+              {...register('lastName', {
+                required: 'Last name cannot be blank',
+                maxLength : {
+                  value: 30,
+                  message: 'Last name cannot be larger than 30 characters.'
+                },
+                pattern: {
+                  value: /[0-9a-z]/,
+                  message: 'Last name must consist of alphanumeric (A-Z and 0-9) characters.'
+                }
+              })}
               type='text'
               className='text-input'
               id='signup-lastname-input'
-              onChange={updateLastName}
-              required
             ></input>
-            <span className='text-input-error'>{lastName.errMsg}</span>
+            <ErrorMessage errors={ errors } name="lastName">
+              {({ messages }) =>
+                messages &&
+                Object.entries(messages).map(([type, message]) => (
+                  <p key={type} className='text-input-error'>{message}</p>
+                ))
+              }
+            </ErrorMessage>
           </div>
           
           <div className='input-container'>
             <label htmlFor='signup-username-input'>Birth Date</label>
             <input
+              {...register('birthDate', {
+                required: 'A valid birthday must be selected',
+                maxLength : {
+                  value: 10,
+                  message: 'Birthday format is invalid (wrong length)'
+                },
+                pattern: {
+                  value: /[\d-]/,
+                  message: 'Birthday format is invalid (wrong characters).'
+                }
+              })}
               type='date'
               className='date-input'
               id='signup-birthday-input'
-              onChange={updateBirthday}
-              required
             ></input>
+            <ErrorMessage errors={ errors } name="birthDate">
+              {({ messages }) =>
+                messages &&
+                Object.entries(messages).map(([type, message]) => (
+                  <p key={type} className='text-input-error'>{message}</p>
+                ))
+              }
+            </ErrorMessage>
           </div>
           <div className='btn-container'>
-            <button
-              className={formCompleted ? 'button': 'button disabled-btn'}
-              disabled={!formCompleted}
-            >
-              Sign Up
-            </button>
+            <button className={ isValid ? 'button': 'button disabled-btn' }> Sign Up </button>
           </div>
         </form>
       </div>
